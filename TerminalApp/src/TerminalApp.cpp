bbfaf54769ca2bdca23715804a46743441ca7bc2
maierjan <maierjan@informatik.hu-berlin.de>
2020-11-04 16:55:39 +0100

    First Terminal structure

--- TerminalApp/src/TerminalApp.cpp
+++ TerminalApp/src/TerminalApp.cpp
@@ -1,19 +1,28 @@
-#include <RayCore.h>
+#include "TerminalApp.h"
 
-class TerminalApp : public RAY::Application
+
+
+TerminalApp::TerminalApp()
 {
-public:
-	TerminalApp()
-	{
 
-	}
+}
 
-	~TerminalApp()
-	{
+TerminalApp::~TerminalApp()
+{
 
-	}
+}
 
-};
+void TerminalApp::Run()
+{
+	while (true);
+
+	// do config stuff
+	// create terminal
+	// run terminal
+		// wait for commands
+		// on command do stuff with beamline, tracer, analyser
+	// on exit close terminal
+}
 
 RAY::Application* RAY::CreateApplication()
 {
