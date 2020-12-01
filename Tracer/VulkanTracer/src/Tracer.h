af377e161593e82dc349563faed09456f9213d54
Enrico Ahlers <enrico.ahlers@helmholtz-berlin.de>
2020-12-01 20:44:40 +0100

    fix in Tracer.h

--- Tracer/VulkanTracer/src/Tracer.h
+++ Tracer/VulkanTracer/src/Tracer.h
@@ -41,66 +41,66 @@ class vkComputeApplication {
         void run();
 
     private:
-	VkInstance instance;
-	VkDebugUtilsMessengerEXT debugMessenger;
-	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
-	VkDevice device;
-	uint32_t outputBufferSize;
-	VkBuffer outputBuffer;
-	VkDeviceMemory outputBufferMemory;
-	uint32_t inputBufferSize;
-	VkBuffer inputBuffer;
-	VkDeviceMemory inputBufferMemory;
-	VkPipeline pipeline;
-	VkPipelineLayout pipelineLayout;
-	VkShaderModule computeShaderModule;
-	VkCommandPool commandPool;
-	VkCommandBuffer commandBuffer;
-	VkDescriptorPool descriptorPool;
-	VkDescriptorSet descriptorSet;
-	VkDescriptorSetLayout descriptorSetLayout;
-	VkQueue computeQueue;
-	uint32_t queueFamilyIndex;
-	uint32_t rayAmount;
-	std::vector<Ray> rayVector;
-    struct QueueFamilyIndices {
-        uint32_t computeFamily;
-        bool hasvalue;
+        VkInstance instance;
+        VkDebugUtilsMessengerEXT debugMessenger;
+        VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+        VkDevice device;
+        uint32_t outputBufferSize;
+        VkBuffer outputBuffer;
+        VkDeviceMemory outputBufferMemory;
+        uint32_t inputBufferSize;
+        VkBuffer inputBuffer;
+        VkDeviceMemory inputBufferMemory;
+        VkPipeline pipeline;
+        VkPipelineLayout pipelineLayout;
+        VkShaderModule computeShaderModule;
+        VkCommandPool commandPool;
+        VkCommandBuffer commandBuffer;
+        VkDescriptorPool descriptorPool;
+        VkDescriptorSet descriptorSet;
+        VkDescriptorSetLayout descriptorSetLayout;
+        VkQueue computeQueue;
+        uint32_t queueFamilyIndex;
+        uint32_t rayAmount;
+        std::vector<Ray> rayVector;
+        struct QueueFamilyIndices {
+            uint32_t computeFamily;
+            bool hasvalue;
 
-        bool isComplete() {
-            return hasvalue;
-        }
-    };
-    QueueFamilyIndices QueueFamily;
+            bool isComplete() {
+                return hasvalue;
+            }
+        };
+        QueueFamilyIndices QueueFamily;
 
-    void initVulkan();
-    void mainLoop();
-    void cleanup();
-    void createInstance();
-    void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
-    void setupDebugMessenger();
-    std::vector<const char*> getRequiredExtensions();
-    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData);
-    bool checkValidationLayerSupport();
-    void pickPhysicalDevice();
-    bool isDeviceSuitable(VkPhysicalDevice device);
-    int rateDevice(VkPhysicalDevice device);
-    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
-    void createLogicalDevice()
-    uint32_t findMemoryType(uint32_t memoryTypeBits, VkMemoryPropertyFlags properties)
-    void createOutputBuffer()
-    void createInputBuffer()
-    void fillInputBuffer()
-    void createDescriptorSetLayout()
-    void createDescriptorSet()
-    uint32_t* readFile(uint32_t& length, const char* filename)
-    void createComputePipeline()
-    void createCommandBuffer()
-    void runCommandBuffer()
-    void setRayAmount(uint32_t inputRayAmount)
-    void readDataFromOutputBuffer()
-    void generateRays()
+        void initVulkan();
+        void mainLoop();
+        void cleanup();
+        void createInstance();
+        void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+        void setupDebugMessenger();
+        std::vector<const char*> getRequiredExtensions();
+        static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData);
+        bool checkValidationLayerSupport();
+        void pickPhysicalDevice();
+        bool isDeviceSuitable(VkPhysicalDevice device);
+        int rateDevice(VkPhysicalDevice device);
+        QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+        void createLogicalDevice();
+        uint32_t findMemoryType(uint32_t memoryTypeBits, VkMemoryPropertyFlags properties);
+        void createOutputBuffer();
+        void createInputBuffer();
+        void fillInputBuffer();
+        void createDescriptorSetLayout();
+        void createDescriptorSet();
+        uint32_t* readFile(uint32_t& length, const char* filename);
+        void createComputePipeline();
+        void createCommandBuffer();
+        void runCommandBuffer();
+        void setRayAmount(uint32_t inputRayAmount);
+        void readDataFromOutputBuffer();
+        void generateRays();
 
-    int main()
+        int main();
 
 }
