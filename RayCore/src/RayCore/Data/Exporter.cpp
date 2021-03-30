bd54e93e4cfaaa64ac44323cab37012455ad0752
Jannis <jannismaier97@gmail.com>
2021-03-30 10:17:06 +0200

    Added rapidxml lib Starting to create data framework for loading and saving ray projects

---
+++ RayCore/src/RayCore/Data/Exporter.cpp
@@ -0,0 +1,28 @@
+#include "Exporter.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include "rapidxml.hpp"
+
+
+namespace RAY
+{
+    Exporter::Exporter()
+    {
+
+    }
+
+    Exporter::~Exporter()
+    {
+
+    }
+
+    void Exporter::exportToXML()
+    {
+        std::string text = "Das ist ein Test!";
+        std::vector<char> cstr(text.c_str(), text.c_str() + text.size() + 1);
+        rapidxml::xml_document<> doc;
+        doc.parse<0>(cstr.data());
+    }
+}
--- TerminalApp/TerminalApp.h
+++ TerminalApp/TerminalApp.h
@@ -1,14 +1,14 @@
 #pragma once 
 #include "RayCore.h"
-using namespace std;
-
-
 
 class TerminalApp : public RAY::Application
 {
 public:
 	TerminalApp();
+	TerminalApp(int argc, char** argv);
 	~TerminalApp();
+
+	const std::string& getProvidedFilePath() const { return providedFile; };
 
 private:
 	// int executeCommand(char** argv);
@@ -17,5 +17,8 @@ private:
 	// int loadCMD(char** argv);
 	// int runCMD(char** argv);
 	// int exitCMD(char** argv);
+	int m_argc;
+	char** m_argv;
 
+	std::string providedFile;
 };
