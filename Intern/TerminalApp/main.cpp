9f927934f6b8071a00557e61bc81c2b318378ef4
Jannis Maier <jannismaier97@gmail.com>
2021-06-17 13:29:00 +0200

    Linux fix

--- Intern/TerminalApp/main.cpp
+++ Intern/TerminalApp/main.cpp
@@ -4,8 +4,10 @@
 
 int main(int argc, char** argv)
 {
+#ifdef RAY_PLATFORM_WINDOWS
 	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
 
 	TerminalApp app = TerminalApp(argc, argv);
 	app.run(/*app.getProvidedFilePath()*/);
