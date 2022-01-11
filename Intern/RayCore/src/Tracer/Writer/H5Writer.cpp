c3638f087ee26d9b71e8d5cae67f0c998b6ea687
Rudi Schneider <schneider-rudi@protonmail.ch>
2022-01-11 12:18:02 +0100

    fixed potential bug in H5Writer

--- Intern/RayCore/src/Tracer/Writer/H5Writer.cpp
+++ Intern/RayCore/src/Tracer/Writer/H5Writer.cpp
@@ -1,9 +1,13 @@
 #include "H5Writer.hpp"
 
+#include <sstream>
+
 H5Writer::H5Writer() : m_file("output.h5", H5Easy::File::Overwrite) {}
 
 void H5Writer::appendRays(const std::vector<double>& outputRays, size_t index) {
-    std::string s = "/data/" + index;
+    std::stringstream ss;
+    ss << "/data/";
+    ss << index;
 
-    H5Easy::dump(m_file, s, outputRays);
+    H5Easy::dump(m_file, ss.str(), outputRays);
 }
