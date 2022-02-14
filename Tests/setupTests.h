45c66371ab7e6564c687720c80dc7a5f46f1ddfd
Rudi Schneider <schneider-rudi@protonmail.ch>
2022-02-14 16:08:13 +0100

    finished CHECK_EQ system

--- Tests/setupTests.h
+++ Tests/setupTests.h
@@ -1,5 +1,6 @@
 #include "Core.h"
 #include "Data/Importer.h"
+#include "Debug.h"
 #include "Model/Beamline/Objects/Objects.h"
 #include "Model/Geometry/Geometry.h"
 #include "Tracer/Ray.h"
@@ -7,7 +8,6 @@
 #include "UserParameter/WorldUserParams.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-#include "Debug.h"
 
 // Run Test Macros
 #define RUN_TEST_GRATINGS 1
@@ -65,36 +65,43 @@
     EXPECT_ITERABLE_BASE(EXPECT_DOUBLE_EQ, ARR(L), ARR(L), ref, target)
 //
 
-
 // new EXPECT system:
+/* usage:
+    CHECK_EQ(A, B, 1e-10); // with explicit tolerance
+    CHECK_EQ(A, B);        // without default tolerance
+*/
 
 template <int N, int M>
 inline std::vector<double> to_vec_impl(glm::mat<N, M, double> arg) {
-	std::vector<double> out(N*M);
-	for (size_t i = 0; i < N*M; i++) {
-		out[i] = arg[i/N][i%N];
-	}
-	return out;
+    std::vector<double> out(N * M);
+    for (size_t i = 0; i < N * M; i++) {
+        out[i] = arg[i / N][i % N];
+    }
+    return out;
 }
 
 template <int N>
 inline std::vector<double> to_vec_impl(glm::vec<N, double> arg) {
-	std::vector<double> out(N);
-	for (size_t i = 0; i < N; i++) {
-		out[i] = arg[i];
-	}
-	return out;
+    std::vector<double> out(N);
+    for (size_t i = 0; i < N; i++) {
+        out[i] = arg[i];
+    }
+    return out;
 }
 
 template <size_t N>
 inline std::vector<double> to_vec_impl(std::array<double, N> arg) {
-	std::vector<double> out(N);
-	for (size_t i = 0; i < N; i++) {
-		out[i] = arg[i];
-	}
-	return out;
+    std::vector<double> out(N);
+    for (size_t i = 0; i < N; i++) {
+        out[i] = arg[i];
+    }
+    return out;
 }
 
-void check_eq(std::string filename, int line, std::string l, std::string r, std::vector<double> vl, std::vector<double> vr, double tolerance=1e-10);
+void check_eq(std::string filename, int line, std::string l, std::string r,
+              std::vector<double> vl, std::vector<double> vr,
+              double tolerance = 1e-10);
 
-#define CHECK_EQ(L, R) check_eq(__FILE__, __LINE__, #L, #R, to_vec_impl(L), to_vec_impl(R)) // TODO tolerance
+#define CHECK_EQ(L, R, ...)                                              \
+    check_eq(__FILE__, __LINE__, #L, #R, to_vec_impl(L), to_vec_impl(R), \
+             ##__VA_ARGS__)  // __VA_ARGS__ = tolerance or nothing
