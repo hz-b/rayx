ca164130983d0699c081b0f82ffb61a8ad921024
Jannis Maier <jannis.maier@helmholtz-berlin.de>
2022-04-13 16:45:12 +0200

    Revert "Merge branch 'development' of gitlab.helmholtz-berlin.de:RAY/RAY-X into development"
    This reverts commit d8d6686ea605de003f51210e178b3aa403844b66.

--- Intern/RayCore/src/utils.cpp
+++ Intern/RayCore/src/utils.cpp
@@ -104,10 +104,6 @@ std::array<double, 4> glmToArray4(glm::dvec4 v) {
     return a;
 }
 
-glm::dvec4 arrayToGlm4(std::array<double, 4> v) {
-    return glm::dvec4(v[0], v[1], v[2], v[3]);
-}
-
 /**
  * @brief Moves Source Vector at the end of destination Vector.
  *
