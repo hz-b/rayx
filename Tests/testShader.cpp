17eac0a2022e3daae460f822217221c469c0c6b8
memoryleak47 <memoryleak47@protonmail.ch>
2022-07-07 11:59:40 +0200

    added testUniformRandom

--- Tests/testShader.cpp
+++ Tests/testShader.cpp
@@ -6,10 +6,21 @@
 namespace RAYX {
 	namespace CPP_TRACER {
 		double r8_exp(double);
+		double squaresDoubleRNG(uint64_t&);
 	}
 }
 
 using namespace RAYX::CPP_TRACER;
+
+TEST_F(TestSuite, testUniformRandom) {
+	uint64_t ctr = 13;
+	for (int i = 0; i < 100; i++) {
+		double d = squaresDoubleRNG(ctr);
+		if (d < 0.0 || d > 1.0) {
+			ADD_FAILURE() << "invalid d";
+		}
+	}
+}
 
 TEST_F(TestSuite, ExpTest) {
 	std::vector<double> args = {10.0, 5.0, 2.0, 1.0, 0.5, 0.0001, 0.0};
