21ab19806a094fa53cfd67ae7e94f15dfe929d35
memoryleak47 <memoryleak47@protonmail.ch>
2022-07-07 11:41:39 +0200

    re-added ExpTest

--- Tests/testShader.cpp
+++ Tests/testShader.cpp
@@ -1,3 +1,20 @@
 #include "setupTests.h"
+#include "Tracer/CpuTracer.h"
 
 // TODO(rudi): shader tests
+
+namespace RAYX {
+	namespace CPP_TRACER {
+		double r8_exp(double);
+	}
+}
+
+using namespace RAYX::CPP_TRACER;
+
+TEST_F(TestSuite, ExpTest) {
+	std::vector<double> args = {10.0, 5.0, 2.0, 1.0, 0.5, 0.0001, 0.0};
+	for (auto x : args) {
+		CHECK_EQ(r8_exp(x), exp(x));
+		CHECK_EQ(r8_exp(-x), exp(-x));
+	}
+}
