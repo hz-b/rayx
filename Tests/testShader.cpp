7f1c0f6135e6d720fe83d7c870bd0ea9328a8ba7
memoryleak47 <memoryleak47@protonmail.ch>
2022-07-07 12:26:09 +0200

    added LogTest

--- Tests/testShader.cpp
+++ Tests/testShader.cpp
@@ -6,6 +6,7 @@
 namespace RAYX {
 	namespace CPP_TRACER {
 		double r8_exp(double);
+		double r8_log(double);
 		double squaresDoubleRNG(uint64_t&);
 	}
 }
@@ -33,5 +34,13 @@ TEST_F(TestSuite, ExpTest) {
 	for (auto x : args) {
 		CHECK_EQ(r8_exp(x), exp(x));
 		CHECK_EQ(r8_exp(-x), exp(-x));
+	}
+}
+
+// interestingly r8_log and log behave differently on input 0, namely 1 vs -inf.
+TEST_F(TestSuite, LogTest) {
+	std::vector<double> args = {10.0, 5.0, 2.0, 1.0, 0.5, 0.0001, 0.0000001};
+	for (auto x : args) {
+		CHECK_EQ(r8_log(x), log(x));
 	}
 }
