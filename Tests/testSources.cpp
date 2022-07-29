7b57fb206e5ec702712138b5658bde3595cb6503
memoryleak47 <memoryleak47@protonmail.ch>
2022-07-29 12:15:35 +0200

    removed obsolete TODO

--- Tests/testSources.cpp
+++ Tests/testSources.cpp
@@ -1,7 +1,5 @@
 #include "setupTests.h"
 
-// TODO(Rudi) test other sources
-
 TEST_F(TestSuite, MatrixSource) {
     auto beamline = loadBeamline("MatrixSource");
     auto a = beamline.getInputRays();
