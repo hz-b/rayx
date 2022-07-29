97935eede7d3cc74902798bdb03e408feef4293d
memoryleak47 <memoryleak47@protonmail.ch>
2022-07-29 12:38:00 +0200

    unified testSources

--- Tests/testSources.cpp
+++ Tests/testSources.cpp
@@ -7,10 +7,8 @@ TEST_F(TestSuite, MatrixSource) {
     compareRayLists(a, b);
 }
 
-TEST_F(TestSuite, PointSourceHardEdge) {
-    auto rays = loadBeamline("PointSourceHardEdge").getInputRays();
-    double sourceEnergy = 120.97;
-    double energySpread = 12.1;
+void checkDistribution(const RayList& rays, double sourceEnergy,
+                       double energySpread) {
     CHECK_EQ(rays.rayAmount(), 200);
     for (auto r : rays) {
         double min_e = sourceEnergy - energySpread;
@@ -20,35 +18,19 @@ TEST_F(TestSuite, PointSourceHardEdge) {
             RAYX_ERR << "range is " << min_e << " to " << max_e;
         }
     }
+}
+
+TEST_F(TestSuite, PointSourceHardEdge) {
+    auto rays = loadBeamline("PointSourceHardEdge").getInputRays();
+    checkDistribution(rays, 120.97, 12.1);
 }
 
 TEST_F(TestSuite, PointSourceSoftEdge) {
     auto rays = loadBeamline("PointSourceSoftEdge").getInputRays();
-    double sourceEnergy = 151;
-    double energySpread = 6;
-    CHECK_EQ(rays.rayAmount(), 200);
-    for (auto r : rays) {
-        double min_e = sourceEnergy - energySpread;
-        double max_e = sourceEnergy + energySpread;
-        if (r.m_energy > max_e || r.m_energy < min_e) {
-            RAYX_WARN << "energy out of range: " << r.m_energy;
-            RAYX_ERR << "range is " << min_e << " to " << max_e;
-        }
-    }
+    checkDistribution(rays, 151, 6);
 }
 
 TEST_F(TestSuite, MatrixSourceEnergyDistribution) {
     auto rays = loadBeamline("MatrixSourceSpreaded").getInputRays();
-
-    double sourceEnergy = 42;
-    double energySpread = 10;
-    CHECK_EQ(rays.rayAmount(), 200);
-    for (auto r : rays) {
-        double min_e = sourceEnergy - energySpread;
-        double max_e = sourceEnergy + energySpread;
-        if (r.m_energy > max_e || r.m_energy < min_e) {
-            RAYX_WARN << "energy out of range: " << r.m_energy;
-            RAYX_ERR << "range is " << min_e << " to " << max_e;
-        }
-    }
+    checkDistribution(rays, 42, 10);
 }
