ae32fc0353cca5772884598d0c349b9e3e519751
memoryleak47 <memoryleak47@protonmail.ch>
2022-08-21 17:23:28 +0200

    fixed Ellipsoid_MB

--- Tests/testIntegration.cpp
+++ Tests/testIntegration.cpp
@@ -91,10 +91,10 @@ TEST_F(TestSuite, toroid) { compareAgainstRayUI("toroid"); }
 TEST_F(TestSuite, toroid_swapped) { compareAgainstRayUI("toroid_swapped"); }
 
 TEST_F(TestSuite, Ellipsoid_DGIA) { compareAgainstRayUI("Ellipsoid_DGIA"); }
+TEST_F(TestSuite, Ellipsoid_MB) { compareAgainstRayUI("Ellipsoid_MB"); }
 
 // TODO(Rudi): fix and re-enable tests, Why do they fail?
 /*
-TEST_F(TestSuite, Ellipsoid_MB) { compareAgainstRayUI("Ellipsoid_MB"); }
 TEST_F(TestSuite, Ellipsoid_PM) { compareAgainstRayUI("Ellipsoid_PM"); }
 
 TEST_F(TestSuite, ellipsoid_ip_200ellipsmis_simpl) { // this is a temporary
