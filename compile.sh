ffd207597a1b7b834d8c5a1e723581d01169e52f
memoryleak47 <memoryleak47@protonmail.ch>
2022-09-21 18:23:59 +0200

    make compile.sh more robust

--- compile.sh
+++ compile.sh
@@ -2,6 +2,13 @@
 
 [[ ! -d .git ]] && echo "You need to be in the root directory of the repo" && exit
 
+trap 'rm ./build/mode' SIGINT
+
+echo "Note that it's discouraged to switch between using compile.sh and VSCode calling CMake"
+echo "Combining both might lead to an illegal state."
+echo "In this case you need to remove the build folder once."
+echo
+
 mode=Debug
 
 for var in "$@"
@@ -20,10 +27,11 @@ done
 echo Updating git submodules ...
 git submodule update --recursive
 
-# setup build folder
-if [[ ! -d "./build" ]]; then
-    echo Setting up build directory ... 
+if [ ! -f ./build/mode ] || [[ ! "$(cat ./build/mode)" == "$mode" ]]; then
+    echo Setting up build directory for mode $mode ...
     cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=$mode -B ./build -G Ninja
+
+    echo $mode > ./build/mode
 fi
 
 # compiling
