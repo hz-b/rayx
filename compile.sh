368fde220a040ce7e7827e3c56e7c1c578336494
memoryleak47 <memoryleak47@protonmail.ch>
2022-09-21 18:53:06 +0200

    compile.sh SIGINT improvements

--- compile.sh
+++ compile.sh
@@ -2,8 +2,6 @@
 
 [[ ! -d .git ]] && echo "You need to be in the root directory of the repo" && exit
 
-trap 'rm ./build/mode' SIGINT
-
 echo "Note that it's discouraged to switch between using compile.sh and VSCode calling CMake"
 echo "Combining both might lead to an illegal state."
 echo "In this case you need to remove the build folder once."
@@ -28,6 +26,8 @@ echo Updating git submodules ...
 git submodule update --recursive
 
 if [ ! -f ./build/mode ] || [[ ! "$(cat ./build/mode)" == "$mode" ]]; then
+    echo > ./build/mode
+
     echo Setting up build directory for mode $mode ...
     cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=$mode -B ./build -G Ninja
 
