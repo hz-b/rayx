834556a9401c60619de198568d74b9d72300c703
memoryleak47 <memoryleak47@protonmail.ch>
2022-10-23 19:25:46 +0200

    pre-commit formatting now checks md5hash

--- format.sh
+++ format.sh
@@ -2,4 +2,17 @@
 
 [[ ! -d .git ]] && echo "You need to be in the root directory of the repo" && exit
 
-find Intern Tests '(' -name "*.cpp" -or -name "*.h" ')' -exec clang-format -i {} \;
+changed=0
+
+for f in $(find Intern Tests -name "*.cpp" -or -name "*.h")
+do
+    oldhash=$(md5sum "$f")
+    clang-format -i "$f"
+    newhash=$(md5sum "$f")
+    if [[ ! "$oldhash" == "$newhash" ]]; then
+        echo Formatting fixed: $f
+        changed=1
+    fi
+done
+
+exit $changed
