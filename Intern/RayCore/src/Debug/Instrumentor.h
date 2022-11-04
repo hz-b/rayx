7a72137a58086e90563665aef9d8c3c9ad540af3
Jannis Maier <jannismaier97@gmail.com>
2022-11-04 09:52:50 +0100

    bench readability

--- Intern/RayCore/src/Debug/Instrumentor.h
+++ Intern/RayCore/src/Debug/Instrumentor.h
@@ -115,7 +115,7 @@ class RAYX_API InstrumentationTimer {
         if (m_canPrint && BENCH_FLAG) {
             long long duration = end - start;
             double seconds = duration * 0.000001;
-            std::cout << "BENCH: " << m_Name << ": " << seconds << "s" << std::endl;
+            std::cout << "BENCH: " << m_Name << ": " << std::endl << seconds << "s" << std::endl;
         }
 
         m_isStopped = true;
