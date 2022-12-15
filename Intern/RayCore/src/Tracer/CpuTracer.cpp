dffa448be18dac07be8787a0f34c60aba58ca04a
memoryleak47 <memoryleak47@protonmail.ch>
2022-12-15 14:13:14 +0100

    fix obsolete quadricData prefix; prepare random seeded shader

--- Intern/RayCore/src/Tracer/CpuTracer.cpp
+++ Intern/RayCore/src/Tracer/CpuTracer.cpp
@@ -8,6 +8,7 @@
 #include "Debug/Instrumentor.h"
 #include "Material/Material.h"
 #include "Model/Beamline/OpticalElement.h"
+#include "Random.h"
 
 namespace RAYX {
 
@@ -25,10 +26,7 @@ std::vector<Ray> CpuTracer::trace(const Beamline& beamline) {
 
     auto rayList = beamline.getInputRays();
 
-    CPU_TRACER::numberOfBeamlines = 1;
-    CPU_TRACER::numberOfElementsPerBeamline = (double)beamline.m_OpticalElements.size();
-    CPU_TRACER::numberOfRays = (double)rayList.size();
-    CPU_TRACER::numberOfRaysPerBeamLine = (double)rayList.size();
+    CPU_TRACER::randomSeed = randomDouble();
 
     CPU_TRACER::quadricData.data.clear();
     CPU_TRACER::xyznull.data.clear();
--- Intern/RayCore/src/Tracer/VulkanTracer.cpp
+++ Intern/RayCore/src/Tracer/VulkanTracer.cpp
@@ -7,6 +7,7 @@
 
 #include "Debug/Debug.h"
 #include "Debug/Instrumentor.h"
+#include "Random.h"
 #include "RayCore.h"
 
 #ifdef RAYX_PLATFORM_MSVC
@@ -36,13 +37,10 @@ std::vector<Ray> VulkanTracer::trace(const Beamline& beamline) {
     }
 
     auto rayList = beamline.getInputRays();
+    const uint32_t numberOfRays = rayList.size();
 
-    uint32_t numberOfBeamlines = 1;
-    uint32_t numberOfQuadricsPerBeamline = (uint32_t)beamline.m_OpticalElements.size();
-    uint32_t numberOfRays = (uint32_t)rayList.size();
-    uint32_t numberOfRaysPerBeamline = numberOfRays;
-    std::vector<double> beamlineData = {(double)numberOfBeamlines, (double)numberOfQuadricsPerBeamline, (double)numberOfRays,
-                                        (double)numberOfRaysPerBeamline};
+    double randomSeed = randomDouble();
+    std::vector<double> beamlineData = {randomSeed, 0, 0, 0};
 
     for (const auto& e : beamline.m_OpticalElements) {
         std::vector<glm::dmat4x4> mats = {e->getSurfaceParams(), e->getInMatrix(), e->getOutMatrix(), e->getObjectParameters(),
