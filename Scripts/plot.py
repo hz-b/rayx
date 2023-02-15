f3776f88ec0dcb57376cbfa9056960f3ec177f6c
memoryleak47 <memoryleak47@protonmail.ch>
2023-02-15 17:31:44 +0100

    impl radio buttons for plot.py

--- Scripts/plot.py
+++ Scripts/plot.py
@@ -4,6 +4,7 @@ import h5py
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.widgets import RadioButtons
 
 import sys
 
@@ -23,21 +24,43 @@ def importOutput(filename: str):
     df = df[["Xloc", "Yloc", "Zloc", "lastElement"]]
     return df
 
+
+BAR = None
+
 def plot(filename: str):
     df = importOutput(filename)
     elems = df["lastElement"].unique()
-    for e in elems:
-        fig = plt.figure()
+    int_elems = list(map(int, elems))
+
+    fig, ax = plt.subplots()
+
+    rax = fig.add_axes([0.02, 0.7, 0.1, 0.15])
+    radio = RadioButtons(rax, int_elems)
+
+    def react(e):
+        global BAR
+        ax.clear()
+        e = float(e)
         d = df[df["lastElement"] == e]
+
         # we don't know whether the element is in the XY or XZ plane,
         # this `relevance` tests which axis is more important.
         relevance = lambda v: max(v) - min(v)
         Y = relevance(d["Yloc"]) > relevance(d["Zloc"])
-        h = plt.hist2d(d["Xloc"], d["Yloc"] if Y else d["Zloc"], bins=200)
-        fig.colorbar(h[3])
-        plt.title("Footprint of element " + str(int(e)))
-        plt.xlabel("x")
-        plt.ylabel("y" if Y else "z")
+        h = ax.hist2d(d["Xloc"], d["Yloc"] if Y else d["Zloc"], bins=200)
+        if BAR:
+            BAR.remove()
+            BAR = None
+        BAR = plt.colorbar(h[3], ax=ax)
+        ax.set_title("Footprint of element " + str(int(e)))
+        ax.set_xlabel("x")
+        ax.set_ylabel("y" if Y else "z")
+        plt.draw()
+
+    radio.on_clicked(react)
+
+    react(int_elems[0])
+
     plt.show()
 
 if __name__ == "__main__":
