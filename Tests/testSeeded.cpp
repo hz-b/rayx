665cb0277dcb102ba1723f34915f0dce5916baa5
memoryleak47 <memoryleak47@protonmail.ch>
2023-02-15 15:41:55 +0100

    add randomNumbers_normal test

--- Tests/testSeeded.cpp
+++ Tests/testSeeded.cpp
@@ -15,22 +15,38 @@
 
 TEST_F(TestSuite, randomNumbers) {
     RAYX::fixSeed(RAYX::FIXED_SEED);
-    CHECK_EQ(randomDouble(), 0.37454011439684315);
-    CHECK_EQ(randomDouble(), 0.95071431178383392);
-    CHECK_EQ(randomDouble(), 0.73199393850099159);
-    CHECK_EQ(randomDouble(), 0.59865848640880048);
-    CHECK_EQ(randomDouble(), 0.1560186385540335);
-    CHECK_EQ(randomDouble(), 0.15599452381860338);
-    CHECK_EQ(randomDouble(), 0.05808361108835871);
-    CHECK_EQ(randomDouble(), 0.86617614884538952);
-    CHECK_EQ(randomDouble(), 0.60111501151721813);
-    CHECK_EQ(randomDouble(), 0.70807257846651428);
+    for (int i = 0; i < 4; i++) {
+        std::cout << randomDouble() << std::endl;
+    }
+
+    RAYX::fixSeed(RAYX::FIXED_SEED);
+    double r;
+    r = randomDouble();
+    CHECK_EQ(r, 0.37454011439684315);
+    r = randomDouble();
+    CHECK_EQ(r, 0.79654298438610116);
+    r = randomDouble();
+    CHECK_EQ(r, 0.95071431178383392);
+    r = randomDouble();
+    CHECK_EQ(r, 0.1834347877147223);
 }
 
-TEST_F(TestSuite, randomNumbers2) {
+TEST_F(TestSuite, randomNumbers_normal) {
     RAYX::fixSeed(RAYX::FIXED_SEED);
-    // TODO fix this!
-    // CHECK_EQ(randomNormal(0, 1), -0.55023449442049355);
+    for (int i = 0; i < 4; i++) {
+        std::cout << randomNormal(0, 1) << std::endl;
+    }
+
+    RAYX::fixSeed(RAYX::FIXED_SEED);
+    double r;
+    r = randomNormal(0, 1);
+    CHECK_EQ(r, 0.40402608730396244);
+    r = randomNormal(0, 1);
+    CHECK_EQ(r, 0.12913107166033977);
+    r = randomNormal(0, 1);
+    CHECK_EQ(r, 0.14650860797333812);
+    r = randomNormal(0, 1);
+    CHECK_EQ(r, -0.83114042984660008);
 }
 
 TEST_F(TestSuite, PointSource_seeded) {
