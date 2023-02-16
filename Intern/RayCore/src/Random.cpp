c1dc3b1fdf0808aba921dfa97181639e204feec6
Jannis Maier <jannismaier97@gmail.com>
2023-02-16 10:32:54 +0100

    fixing ci v2

--- Intern/RayCore/src/Random.cpp
+++ Intern/RayCore/src/Random.cpp
@@ -34,7 +34,11 @@ double randomDoubleInRange(double a, double b) {
 // https://stackoverflow.com/questions/38532927/why-gcc-and-msvc-stdnormal-distribution-are-different
 double randomNormal(double mu, double sigma) {
     constexpr double epsilon = std::numeric_limits<double>::epsilon();
-    constexpr double two_pi = 2.0 * (double)PI;
+#ifdef RAYX_PLATFORM_MSVC // TODO(Jannis): we need to change constants.h so this is not necessary
+    constexpr double two_pi = 2.0 * PI;
+#else
+    constexpr double two_pi = 2.0 * M_PI;
+#endif
 
     // create two random numbers, make sure u1 is greater than epsilon
     double u1, u2;
