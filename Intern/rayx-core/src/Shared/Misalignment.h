a24103fe127efeda3bc5560ae5021d95e367bd32
memoryleak47 <memoryleak47@protonmail.ch>
2023-07-18 15:46:41 +0200

    fix RZPLineDensity coordinate system

--- Intern/rayx-core/src/Shared/Misalignment.h
+++ Intern/rayx-core/src/Shared/Misalignment.h
@@ -1,6 +1,8 @@
 #ifndef MISALIGNMENT_H
 #define MISALIGNMENT_H
 
+#include "Ray.h"
+
 struct Misalignment {
     double m_translationXerror;
     double m_translationYerror;
@@ -11,5 +13,25 @@ struct Misalignment {
     double m_rotationYerror;
     double m_rotationZerror;
 };
+
+// TODO find adapt.h solution for this.
+#ifndef GLSL
+using Ray = RAYX::Ray;
+#endif
+
+// TODO also apply rotational parts of Misalignment.
+INLINE Ray addMisalignment(Ray r, Misalignment m) {
+    r.m_position.x += m.m_translationXerror;
+    r.m_position.y += m.m_translationYerror;
+    r.m_position.z += m.m_translationZerror;
+    return r;
+}
+
+INLINE Ray subtractMisalignment(Ray r, Misalignment m) {
+    r.m_position.x -= m.m_translationXerror;
+    r.m_position.y -= m.m_translationYerror;
+    r.m_position.z -= m.m_translationZerror;
+    return r;
+}
 
 #endif
