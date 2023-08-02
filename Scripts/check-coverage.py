d4a12cb51f19b6987e0c077d1a25bd7bf5215ec3
Enrico Ahlers <enrico.ahlers@helmholtz-berlin.de>
2023-08-02 11:53:39 +0200

    converage now covers cpu and gpu tracing

--- Scripts/check-coverage.py
+++ Scripts/check-coverage.py
@@ -13,8 +13,6 @@ import os
 import datetime
 import subprocess
 
-
-# Only execute inside IDE with rayx as root
 def checkForTerminal():
     cwd = os.getcwd()
     TerminalApp_Path = "build/bin/debug/rayx-core-tst"
@@ -24,43 +22,28 @@ def checkForTerminal():
 
 def run_command(cmd, cwd=None):
     proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd)
-
-    # While the process is running, read lines from stdout/stderr and print them
     while True:
-        # Read a line from stdout
         output = proc.stdout.readline().decode()
         if output:
             print(output, end='')
-
-        # Read a line from stderr
         err = proc.stderr.readline().decode()
         if err:
             print(err, end='')
-
-        # If both stdout and stderr were empty and the process has ended, break the loop
         if output == '' and err == '' and proc.poll() is not None:
             break
-
-    # Wait for the process to finish, and get the final exit code
     exitcode = proc.wait()
-
     if exitcode != 0:
         return False, f"Command exited with code {exitcode}"
     else:
         return True, "Command executed successfully"
 
 
-def main():
-    exists, path = checkForTerminal()
-    if not (exists):
-        print("Check for build!")
-        return
-
-    
+def execute_tests(path, env, flags=[]):
     proc = subprocess.Popen(
-        [path, "-x"],
+        [path] + flags,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
+        env=env
     )
 
     failed_tests = []
@@ -76,38 +59,56 @@ def main():
             if 'Failure' in output:
                 failed_tests.append(current_test)
 
-    if failed_tests:
+    return failed_tests, proc.poll()
+
+
+def main():
+    exists, path = checkForTerminal()
+    if not (exists):
+        print("Check for build!")
+        return
+
+    env = os.environ.copy()
+
+    # Run tests with -x flag
+    env["LLVM_PROFILE_FILE"] = "cpu.profraw"
+    failed_tests_x, exit_code_x = execute_tests(path, env, ["-x"])
+
+    # Run tests without -x flag
+    env["LLVM_PROFILE_FILE"] = "gpu.profraw"
+    failed_tests, exit_code = execute_tests(path, env)
+
+    if failed_tests or failed_tests_x:
         print("\nThe following tests failed:")
-        for failed_test in failed_tests:
+        for failed_test in (failed_tests + failed_tests_x):
             print(failed_test)
 
-    if proc.poll() != 0 or failed_tests:  # if the subprocess didn't exit correctly or if there were failed tests
+    if exit_code != 0 or exit_code_x != 0 or failed_tests or failed_tests_x:
         print("\nSome tests did not execute successfully. Make sure that all tests are passing before analyzing coverage")
         return
 
     try:
-        # Merge and delete default.profraw
-        success, output = run_command('llvm-profdata merge -sparse default.profraw -o default.profdata')
+        # Merge both profraw files and delete them
+        success, output = run_command('llvm-profdata merge -sparse cpu.profraw gpu.profraw -o default.profdata')
         if not success:
             print("Failed to merge profraw: " + output)
             return
 
-        # Define output directory with timestamp
         timestamp = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
         output_dir = f'./Scripts/coverage-outputs/{timestamp}'
         
-        # If the directory doesn't exist, create it
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
-        
-        # Generate coverage and delete default.profdata
+
         success, output = run_command(f'llvm-cov show -format=html -output-dir={output_dir} ./build/bin/debug/rayx-core-tst -instr-profile=default.profdata -object=./build/lib/debug/librayx-core.so')
         if not success:
             print("Failed to generate coverage: " + output)
             return
     finally:
-        if os.path.exists('default.profraw'):
-            os.remove('default.profraw')
+        if os.path.exists('cpu.profraw'):
+            os.remove('cpu.profraw')
+        if os.path.exists('gpu.profraw'):
+            os.remove('gpu.profraw')
         if os.path.exists('default.profdata'):
             os.remove('default.profdata')
 
@@ -116,8 +117,5 @@ def main():
     return
 
 
-# main
 if __name__ == "__main__":
     main()
-
-
