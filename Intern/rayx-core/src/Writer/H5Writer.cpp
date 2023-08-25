9f3d4bd6bf899105dc6686383a813536f5600d3e
Jannis Maier <jannismaier97@gmail.com>
2023-08-25 10:30:45 +0200

    Fix fromDoubles

--- Intern/rayx-core/src/Writer/H5Writer.cpp
+++ Intern/rayx-core/src/Writer/H5Writer.cpp
@@ -61,46 +61,50 @@ void writeH5(const RAYX::BundleHistory& hist, std::string filename, const Format
 }
 
 RAYX::BundleHistory fromDoubles(const std::vector<double>& doubles, const Format& format) {
-    unsigned int formatSize = format.size();  // Now 16 with the inclusion of Ray-ID and Snapshot-ID
-    unsigned int numRays = doubles.size() / formatSize;
+    auto formatSize = format.size();  // Now 16 with the inclusion of Ray-ID and Snapshot-ID
+    auto numRays = doubles.size() / formatSize;
 
     if (doubles.size() % formatSize != 0) {
         throw std::invalid_argument("Size of doubles does not match expected size based on format");
     }
 
-    RAYX::BundleHistory rays;
-    rays.reserve(numRays);
-    unsigned int double_index = 0;
+    RAYX::BundleHistory bundleHist;
+    bundleHist.reserve(numRays);
+    size_t double_index = 0;
+    RAYX::RayHistory rayHist;
     while (double_index < doubles.size()) {
-        RAYX::RayHistory snapshots;
+        // Extract and ignore Ray-ID and Snapshot-ID
+        double rayId = doubles[double_index++];
+        double eventId = doubles[double_index++];
 
-        for (unsigned int snapshot_id = 0; snapshot_id < formatSize; snapshot_id += formatSize) {
-            // Extract and ignore Ray-ID and Snapshot-ID
-            double_index++;  // ray id
-            double_index++;  // snapshot id
-
-            // Extract data for ray
-            glm::dvec3 origin(doubles[double_index], doubles[double_index + 1], doubles[double_index + 2]);
-            double eventType = doubles[double_index + 3];
-            glm::dvec3 direction(doubles[double_index + 4], doubles[double_index + 5], doubles[double_index + 6]);
-            double energy = doubles[double_index + 7];
-            glm::dvec4 stokes(doubles[double_index + 8], doubles[double_index + 9], doubles[double_index + 10], doubles[double_index + 11]);
-            double pathLength = doubles[double_index + 12];
-            double order = doubles[double_index + 13];
-            double lastElement = doubles[double_index + 14];
-
-            RAYX::Ray ray = {
-                origin, eventType, direction, energy, stokes, pathLength, order, lastElement, 0.0,
-            };
-            snapshots.push_back(ray);
-
-            double_index += formatSize - 2;
+        if (eventId == 0) {
+            bundleHist.push_back(rayHist);
+            rayHist.clear();
         }
 
-        rays.push_back(snapshots);
+        // Extract data for ray
+        glm::dvec3 origin(doubles[double_index], doubles[double_index + 1], doubles[double_index + 2]);
+        double eventType = doubles[double_index + 3];
+        glm::dvec3 direction(doubles[double_index + 4], doubles[double_index + 5], doubles[double_index + 6]);
+        double energy = doubles[double_index + 7];
+        glm::dvec4 stokes(doubles[double_index + 8], doubles[double_index + 9], doubles[double_index + 10], doubles[double_index + 11]);
+        double pathLength = doubles[double_index + 12];
+        double order = doubles[double_index + 13];
+        double lastElement = doubles[double_index + 14];
+
+        RAYX::Ray ray = {
+            origin, eventType, direction, energy, stokes, pathLength, order, lastElement, 0.0,
+        };
+
+        rayHist.push_back(ray);
+
+        double_index += formatSize - 2;
     }
 
-    return rays;
+    // Remove first empty snapshot
+    bundleHist.erase(bundleHist.begin());
+
+    return bundleHist;
 }
 
 void readH5(RAYX::BundleHistory& rays, std::string filename, const Format& format) {
