83498d2ef324818d96bfb5c4fe287f0477847c73
a3772 <fanny.zotter@helmholtz-berlin.de>
2023-09-26 18:07:46 +0200

    Seperate Energies and SoftEdge Distribution

--- Intern/rayx-core/src/Beamline/EnergyDistribution.cpp
+++ Intern/rayx-core/src/Beamline/EnergyDistribution.cpp
@@ -1,40 +1,75 @@
 #include "EnergyDistribution.h"
-
+#include "LightSource.h"
 #include "Random.h"
 
 namespace RAYX {
-EnergyDistribution::EnergyDistribution(DatFile df, bool continuous) : m_isContinuous(continuous), m_Variant(df) {}
+EnergyDistribution::EnergyDistribution(DatFile df) :  m_Variant(df) {}
 
-EnergyDistribution::EnergyDistribution(EnergyRange r, bool continuous) : m_isContinuous(continuous), m_Variant(r) {}
+EnergyDistribution::EnergyDistribution(HardEdge he) : m_Variant(he) {}
 
-EnergyDistribution::EnergyDistribution() : EnergyDistribution(EnergyRange(100.0, 0.0), false) {}
+EnergyDistribution::EnergyDistribution(SoftEdge se) : m_Variant(se) {}
+
+EnergyDistribution::EnergyDistribution(SeperateEnergies sep) : m_Variant(sep) {}
+
+EnergyDistribution::EnergyDistribution() : EnergyDistribution(HardEdge(100.0, 0.0)) {}
 
 double EnergyDistribution::selectEnergy() const {
-    // the below code calls either DatFile::selectEnergy or
-    // EnergyRange::selectEnergy depending on what is stored in m_Variant.
-    const auto func = [&](const auto arg) -> double { return arg.selectEnergy(m_isContinuous); };
+    // the below code calls either DatFile::selectEnergy,
+    // HardEdge::selectEnergy, SoftEdge::selectEnergy or SeperateEnergies::selectEnergy
+    // depending on what is stored in m_Variant.
+    const auto func = [&](const auto arg) -> double { return arg.selectEnergy(); };
     return std::visit(func, m_Variant);
 }
 
 double EnergyDistribution::getAverage() const {
     // the below code calls either DatFile::getAverage or
-    // EnergyRange::getAverage depending on what is stored in m_Variant.
+    // HardEdge::getAverage, SoftEdge::getAverage or SeperateEnergies::selectEnergy
+    // depending on what is stored in m_Variant.
     const auto func = [&](const auto arg) -> double { return arg.getAverage(); };
     return std::visit(func, m_Variant);
 }
 
-// EnergyRange impls
+//--------------------------------------------
+// HardEdge impls
 
-EnergyRange::EnergyRange(double centerEnergy, double energySpread) : m_centerEnergy(centerEnergy), m_energySpread(energySpread) {}
+HardEdge::HardEdge(double centerEnergy, double energySpread) : m_centerEnergy(centerEnergy), m_energySpread(energySpread) {}
 
-double EnergyRange::selectEnergy(bool continuous) const {
-    if (continuous) {
-        return randomDoubleInRange(m_centerEnergy - m_energySpread / 2, m_centerEnergy + m_energySpread / 2);
-    } else {
-        double arr[3] = {m_centerEnergy - m_energySpread / 2, m_centerEnergy, m_centerEnergy + m_energySpread / 2};
-        return arr[randomIntInRange(0, 2)];
-    }
+double HardEdge::selectEnergy() const {
+    return randomDoubleInRange(m_centerEnergy - m_energySpread / 2, m_centerEnergy + m_energySpread / 2);
 }
 
-double EnergyRange::getAverage() const { return m_centerEnergy; }
+double HardEdge::getAverage() const { return m_centerEnergy; }
+
+
+//SoftEdge impls
+
+SoftEdge::SoftEdge(double centerEnergy, double sigma) : m_centerEnergy(centerEnergy), m_sigma(sigma) {}
+
+double SoftEdge::selectEnergy() const {
+    return randomNormal(m_centerEnergy, m_sigma);
+}
+
+double SoftEdge::getAverage() const { return m_centerEnergy; }
+
+
+//seperateEnergies impls
+
+SeperateEnergies::SeperateEnergies(double centerEnergy, double energySpread, int numOfEnergies) 
+                : m_centerEnergy(centerEnergy), m_energySpread(energySpread), m_numberOfEnergies(numOfEnergies) {}
+
+
+double SeperateEnergies::getAverage() const { return m_centerEnergy; }
+
+double SeperateEnergies::selectEnergy() const { 
+    
+    double arr[m_numberOfEnergies];
+
+    for (int i = 0; i < m_numberOfEnergies; i++) {
+        arr[i] = (m_centerEnergy - m_energySpread / 2) + i * m_energySpread / (m_numberOfEnergies - 1);
+    }
+
+    return arr[randomIntInRange(0, m_numberOfEnergies-1)];
+
+
+}
 }  // namespace RAYX
