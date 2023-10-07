a8c194a9ebca7a8863af1ff6b14e25b168dd0a8f
a3772 <fanny.zotter@helmholtz-berlin.de>
2023-10-07 13:16:48 +0200

    PixelSource

---
+++ Intern/rayx-core/src/Beamline/Objects/PixelSource.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Beamline/LightSource.h"
+
+namespace RAYX {
+
+class RAYX_API PixelSource : public LightSource {
+  public:
+    PixelSource(const DesignObject&);
+    virtual ~PixelSource() = default;
+
+    std::vector<Ray> getRays() const override;
+
+  private:
+    // Geometric Params
+    SourcePulseType m_sourceDistributionType; // TODO unused.
+
+    double m_linearPol_0;
+    double m_linearPol_45;
+    double m_circularPol;
+
+    double m_verDivergence;
+    double m_sourceDepth;
+    // std::vector<SOURCE_LENGTH> m_source_lengths;
+};
+
+}  // namespace RAYX
