a340a1b5ee17fb4bc24903a511ade990a470ea3c
Jannis Maier <jannismaier97@gmail.com>
2023-10-15 16:42:54 +0200

    Fix crashing on empty text field (imgui)

--- Intern/rayx-ui/src/ImGuiLayer.cpp
+++ Intern/rayx-ui/src/ImGuiLayer.cpp
@@ -170,47 +170,59 @@ void ImGuiLayer::updateImGui(CameraController& camController, FrameInfo& frameIn
         ImGui::ColorEdit3("Color", (float*)&m_ClearColor);  // Edit 3 floats representing a color
 
         ImGui::Text("Camera");
-        ImGui::SliderFloat("FOV", &camController.m_config.FOV, 0.0f, 180.0f);
+        ImGui::SliderFloat("FOV", &camController.m_config.m_FOV, 0.0f, 180.0f);
         ImGui::SliderFloat("Up X", &camController.m_up.x, -1.0f, 1.0f);  // Slider for Up vector x-coordinate
         ImGui::SliderFloat("Up Y", &camController.m_up.y, -1.0f, 1.0f);  // Slider for Up vector y-coordinate
         ImGui::SliderFloat("Up Z", &camController.m_up.z, -1.0f, 1.0f);  // Slider for Up vector z-coordinate
-        ImGui::SliderFloat("Near", &camController.m_config.near, 0.0f, 100.0f);
-        ImGui::SliderFloat("Far", &camController.m_config.far, 0.0f, 10000.0f);
+        ImGui::SliderFloat("Near", &camController.m_config.m_near, 0.0f, 100.0f);
+        ImGui::SliderFloat("Far", &camController.m_config.m_far, 0.0f, 10000.0f);
 
         static char posStrX[32];
         snprintf(posStrX, sizeof(posStrX), "%f", camController.m_position.x);
         if (ImGui::InputText("Position X", posStrX, sizeof(posStrX))) {
-            camController.m_position.x = std::stof(posStrX);
+            if (!std::all_of(posStrX, posStrX + strlen(posStrX), ::isspace)) {
+                camController.m_position.x = std::stof(posStrX);
+            }
         }
 
         static char posStrY[32];
         snprintf(posStrY, sizeof(posStrY), "%f", camController.m_position.y);
         if (ImGui::InputText("Position Y", posStrY, sizeof(posStrY))) {
-            camController.m_position.y = std::stof(posStrY);
+            if (!std::all_of(posStrY, posStrY + strlen(posStrY), ::isspace)) {
+                camController.m_position.y = std::stof(posStrY);
+            }
         }
 
         static char posStrZ[32];
         snprintf(posStrZ, sizeof(posStrZ), "%f", camController.m_position.z);
         if (ImGui::InputText("Position Z", posStrZ, sizeof(posStrZ))) {
-            camController.m_position.z = std::stof(posStrZ);
+            if (!std::all_of(posStrZ, posStrZ + strlen(posStrZ), ::isspace)) {
+                camController.m_position.z = std::stof(posStrZ);
+            }
         }
 
         static char dirStrX[32];
         snprintf(dirStrX, sizeof(dirStrX), "%f", camController.m_direction.x);
         if (ImGui::InputText("Direction X", dirStrX, sizeof(dirStrX))) {
-            camController.m_direction.x = std::stof(dirStrX);
+            if (!std::all_of(dirStrX, dirStrX + strlen(dirStrX), ::isspace)) {
+                camController.m_direction.x = std::stof(dirStrX);
+            }
         }
 
         static char dirStrY[32];
         snprintf(dirStrY, sizeof(dirStrY), "%f", camController.m_direction.y);
         if (ImGui::InputText("Direction Y", dirStrY, sizeof(dirStrY))) {
-            camController.m_direction.y = std::stof(dirStrY);
+            if (!std::all_of(dirStrY, dirStrY + strlen(dirStrY), ::isspace)) {
+                camController.m_direction.y = std::stof(dirStrY);
+            }
         }
 
         static char dirStrZ[32];
         snprintf(dirStrZ, sizeof(dirStrZ), "%f", camController.m_direction.z);
         if (ImGui::InputText("Direction Z", dirStrZ, sizeof(dirStrZ))) {
-            camController.m_direction.z = std::stof(dirStrZ);
+            if (!std::all_of(dirStrZ, dirStrZ + strlen(dirStrZ), ::isspace)) {
+                camController.m_direction.z = std::stof(dirStrZ);
+            }
         }
 
         if (ImGui::Button("Save Camera")) {
