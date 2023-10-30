5fd13f449f4fb13dd8fc14f803c7be78bebea35e
memoryleak47 <memoryleak47@protonmail.ch>
2023-10-30 16:59:49 +0100

    prefer PlaneXZ over a Planar Quadric

--- Intern/rayx-core/src/Beamline/OpticalElement.cpp
+++ Intern/rayx-core/src/Beamline/OpticalElement.cpp
@@ -49,19 +49,7 @@ glm::dmat4 calcTransformationMatrices(glm::dvec4 position, glm::dmat4 orientatio
 double defaultMaterial(const DesignObject& dobj) { return (double)static_cast<int>(dobj.parseMaterial()); }
 
 Surface makePlane() {
-    return serializeQuadric({
-        .m_icurv = 1,
-        .m_a11 = 0,
-        .m_a12 = 0,
-        .m_a13 = 0,
-        .m_a14 = 0,
-        .m_a22 = 0,
-        .m_a23 = 0,
-        .m_a24 = -1,
-        .m_a33 = 0,
-        .m_a34 = 0,
-        .m_a44 = 0,
-    });
+    return serializePlaneXZ();
 }
 
 Surface makeSphere(double radius) {
