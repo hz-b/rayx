9597ae4b776553fe00127d3202c678a0a0906c80
memoryleak47 <memoryleak47@protonmail.ch>
2024-01-04 14:16:07 +0100

    re-doc Ray.h

--- Intern/rayx-core/src/Shader/Ray.h
+++ Intern/rayx-core/src/Shader/Ray.h
@@ -7,32 +7,58 @@
 namespace RAYX {
 #endif
 
-// This type represents a photon ray at a fixed point in time.
-// The most frequently used fields are its *position* (in world coordinates) and the *direction* in which it is currently heading.
-// Often we consider Rays at particular points in time - when *something happens to them*.
-// For example, they could currently be colliding with some OpticalElement.
-// These kind of *events* are encoded using the m_eventType field of the Ray.
-// In EventType.h you can see what each eventType value means.
+/// This type represents a photon ray at a fixed point in time.
+/// Often we consider Rays at particular points in time - when *something happens to them*.
+/// For example, they could currently be colliding with some OpticalElement.
+/// These kind of *events* are encoded using the m_eventType field of the Ray.
+/// In EventType.h you can see what each eventType value means.
+
+struct RAYX_API Ray {
+    /// The position of the ray.
+    /// NOTE: The coordinate system in which the position is to be interpreted, depends on m_eventType!
+    dvec3 m_position;
+
+    /// The m_eventType expresses what is currently happening to the ray.
+    /// During tracing the eventType will be uninitialized (ETYPE_UNINIT).
+    /// Only when an event will be recorded, the m_eventType will be set accordingly.
+    /// See the potential values of `m_eventType` in `EventType.h`.
+    double m_eventType;
+
+    /// The direction of the ray.
+    /// The direction is normalized, so its L2 norm (aka length) is one.
+    /// NOTE: The coordinate system in which direction is to be interpreted, depends on m_eventType!
+    dvec3 m_direction;
+
+    /// The energy of this photon (in eV).
+    double m_energy;
+
+    /// The stokes vector, see https://en.wikipedia.org/wiki/Stokes_parameters
+    /// They express the polarization of the Ray.
+    dvec4 m_stokes;
+
+    /// The distance that this ray has already traveled (in mm).
+    double m_pathLength;
+
+    /// The order of diffraction.
+    /// Is currently only set to a non-zero value by the RZP.
+    double m_order;
+
+    /// the index of the last Element that this ray collided/interacted with.
+    /// is initially set to -1 upon construction by the LightSources.
+    double m_lastElement;
+
+    /// This double only exists to address alignment issues (see below).
+    /// m_padding should never be read or written.
+    double m_padding;
+};
 
 // Note: A `dvec3` needs an alignment of 4 * sizeof(double), hence two dvec3s can never be directly after each other (without padding).
 // Further, the number of doubles in a Ray need to be divisible by four at all times, as we want to store multiple Rays after each other without
 // padding in `rayData`. This is why we need m_padding.
-struct RAYX_API Ray {
-    dvec3 m_position;
-    // The m_eventType is only required for Rays stored as part of a RayHistory or BundleHistory. It then expresses the type of event that occured at
-    // that point in time. For normal rays during tracing the eventType will be undefined. See the potential values of `m_eventType` in `EventType.h`.
-    double m_eventType;
-    dvec3 m_direction;
-    double m_energy;
-    dvec4 m_stokes;
-    double m_pathLength;
-    double m_order;
-    double m_lastElement; // created as -1 by light sources.
+#ifndef GLSL
+static_assert(sizeof(Ray) % alignof(dvec3) == 0);
+#endif
 
-    // This double only exists, so that sizeof(Ray) is divisble by sizeof(dvec4) for alignment reasons.
-    // This should never be read or written.
-    double m_padding;
-};
 
 #ifndef GLSL
 }  // namespace RAYX
