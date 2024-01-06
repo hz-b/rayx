a0b209948271c18447cde42796ccd58aa8b810d6
memoryleak47 <memoryleak47@protonmail.ch>
2024-01-06 21:34:59 +0100

    even more documentation

--- Intern/rayx-core/src/Beamline/OpticalElement.h
+++ Intern/rayx-core/src/Beamline/OpticalElement.h
@@ -19,18 +19,40 @@ struct RAYX_API OpticalElement {
     std::string m_name;
 };
 
+// calculations the transformation matrices that will then be given into the shader.
+// if plane == DesignPlane::XY, it will additionally swap the y and z coordinates to make the shader-internal code consistent.
+// if `calcInMatrix`, this returns the "in-matrix", and otherwise the "out-matrix".
 RAYX_API glm::dmat4 calcTransformationMatrices(glm::dvec4 position, glm::dmat4 orientation, bool calcInMatrix, DesignPlane plane);
+
+// convenience wrapper around calcTransformationMatrices.
 glm::dmat4 defaultInMatrix(const DesignObject& dobj, DesignPlane plane);
 glm::dmat4 defaultOutMatrix(const DesignObject& dobj, DesignPlane plane);
+
+// convenience wrapper around dobj.parseMaterial().
 double defaultMaterial(const DesignObject& dobj);
+
+// constructs an Element given all of its components. Some information that is not explicitly given, will be parsed from the `dobj`.
 Element makeElement(const DesignObject& dobj, Behaviour behaviour, Surface surface, std::optional<Cutout> cutout = {}, DesignPlane plane=DesignPlane::XZ);
+
 Element makeExperts(const DesignObject& dobj);
 Element makeExpertsCubic(const DesignObject& dobj);
+
+// creates a toroid from the parameters given in `dobj`.
 Surface makeToroid(const DesignObject& dobj);
+
+// creates a quadric from the parameters given in `dobj`.
 Surface makeQuadric(const DesignObject& dobj);
+
+// creates a cubic from the parameters given in `dobj`.
 Surface makeCubic(const DesignObject& dobj);
+
+// creates a sphere quadric from the parameters given in `dobj`.
 Surface makeSphere(double radius);
+
+// creates a plane surface.
 Surface makePlane();
+
+// creates a Grating Behaviour from the parameters given in `dobj`.
 Behaviour makeGrating(const DesignObject& dobj);
 
 }  // namespace RAYX
--- Intern/rayx-core/src/Shader/Element.h
+++ Intern/rayx-core/src/Shader/Element.h
@@ -9,26 +9,46 @@
 struct SlopeError {
     double m_sag;  // aka `slopeErrorX`
     double m_mer;  // aka `slopeErrorZ`
-    double m_thermalDistortionAmp;
-    double m_thermalDistortionSigmaX;
-    double m_thermalDistortionSigmaZ;
-    double m_cylindricalBowingAmp;
-    double m_cylindricalBowingRadius;
+    double m_thermalDistortionAmp; // TODO unused
+    double m_thermalDistortionSigmaX; // TODO unused
+    double m_thermalDistortionSigmaZ; // TODO unused
+    double m_cylindricalBowingAmp; // TODO unused
+    double m_cylindricalBowingRadius; // TODO unused
 };
 
 struct Element {
     // for alignment reasons, the dmat4s are at the beginning of the struct.
+
+    // the "in-transformation": it converts a point from the world coordinates to the element coordinates of this element.
+    // The name comes from the fact that an "in-going" ray hitting this elemnet, will first-and-foremost be converted to element coordinates by multiplying with m_inTrans.
     dmat4 m_inTrans;
+
+    // the "out-transformation": it converts a point from element coordinates of this element back to the world coordinates.
+    // This is the matrix inverse to m_inTrans.
     dmat4 m_outTrans;
 
+    // The Behaviour expresses what happens to a ray once it collides with this Element.
     Behaviour m_behaviour;
 
+    // The surface expresses how the Elements surface is curved.
     Surface m_surface;
+
+    // The Cutout limits the Surface to the dimensions of the actual Element.
+    // Surfaces are often infinite, hence we require the Cutout to limit them.
     Cutout m_cutout;
 
-    // general object information
+    // The SlopeError expresses a random noise in the normal vector of a particular surface point on this element.
+    // Such a random noise has the (at least visual) effect, that it feels like a rough - uneven - surface.
+    // As of today, the `applySlopeError` is the only function using this object.
     SlopeError m_slopeError;
+
+    // The azimuthal angle describes the angle at which this element is rotated around the "main-beam" from the previous element to this element.
+    // TODO This is an artifact from RAY-UI, we actually want to get rid of it. (at least it should not be part of the shader model!)
     double m_azimuthalAngle;
+
+    // The material that this object is made of.
+    // See the `enum class Material` from Material.h to make sense of this value.
+    // Materials are either REFLECTIVE, VACUUM or they represent a particular element from the periodic system.
     double m_material;
 
     // This field is unused, it's only there to guarantee that sizeof(Element) is divisible by sizeof(dmat4).
