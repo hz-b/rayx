eddec3bc4f5bad1a51902b9af35e54a3c26430bf
memoryleak47 <memoryleak47@protonmail.ch>
2024-01-30 00:17:22 +0100

    document the intricate details of the RefractiveIndex calculation

--- Intern/rayx-core/src/Shader/RefractiveIndex.cpp
+++ Intern/rayx-core/src/Shader/RefractiveIndex.cpp
@@ -1,14 +1,35 @@
 #include "RefractiveIndex.h"
 #include "InvocationState.h"
 
+// The inv_mat table consists of all the information from the Palik & Nff tables for all materials that were loaded into the shader.
+// All the Palik and Nff entries are all stored as doubles after each other in the inv_mat array.
+// Thus, when you want to get some concrete palik entry for a particular element, you need the inv_matIdx table to "know where to look" in inv_mat:
+// * inv_matIdx[i] is the beginning of the Palik Table in inv_mat for the element with atomic number i+1, for i in [0, 91].
+// * inv_matIdx[i+92] is the beginning of the Nff Table in inv_mat for the element with atomic number i+1, for i in [0, 91].
+// The `i+1` is necessary as atomic numbers start at 1 (Hydrogen), while array indices start at 0.
+// See InvocationState.cpp for more information about these tables.
+
+// If you will, the inv_mat table is a "sparse" 4d-datastructure of shape (2, 92, N, 3).
+// - 2 because we store a Palik-table, and an Nff-table
+// - 92, because we support 92 elements of the periodic table (from Hydrogen to Uranium)
+// - N is the number of entries in a given Palik/Nff table (for a fixed periodic element).
+// - 3 because each Palik / Nff entry consists of three doubles.
+// - It is a "sparse" data structure as most of the entries are actually missing, we only load the actually "used" materials into the shader, not all materials.
+// -- Thus, we cannot index into it as inv_mat[i1][i2][i3][i4] directly, but instead have to use inv_matIdx as described above.
+
+// The concrete layout of inv_mat and inv_matIdx has to be compatible with the "loadMaterialTables" function from Material.cpp
+// It is responsible for creating these tables.
+
+/// The number of palik entries we currently store for this material.
 int RAYX_API getPalikEntryCount(int material) {
     int m = material - 1;  // in [0, 91]
-    // this counts how many doubles are in between the materials index, and the
-    // next index in the table. division by 3, because each entry has 3 members
+    // This counts how many doubles are in between the materials index, and the
+    // next index in the table. Division by 3, because each entry has 3 members
     // currently: energy, n, k, padding.
     return (inv_matIdx[m + 1] - inv_matIdx[m]) / 3;
 }
 
+/// The number of nff entries we currently store for this material.
 int RAYX_API getNffEntryCount(int material) {
     int m = material - 1;  // in [0, 91]
     // the offset of 92 (== number of materials), skips the palik table and
@@ -16,10 +37,11 @@ int RAYX_API getNffEntryCount(int material) {
     return (inv_matIdx[92 + m + 1] - inv_matIdx[92 + m]) / 3;
 }
 
+// Indexes into the palik table of a particular material at a given index.
 PalikEntry RAYX_API getPalikEntry(int index, int material) {
     int m = material - 1;  // in [0, 91]
     // inv_matIdx[m] is the start of the Palik table of material m.
-    // 3*index skips 'index'-many entries.
+    // 3*index skips 'index'-many entries, because an entry consists of 3 doubles.
     int i = inv_matIdx[m] + 3 * index;
 
     PalikEntry e;
@@ -97,6 +119,7 @@ dvec2 RAYX_API getRefractiveIndex(double energy, int material) {
             }
         }
 
+        // compute n, k from the Nff data.
         dvec2 massAndRho = getAtomicMassAndRho(material);
         double mass = massAndRho.x;
         double rho = massAndRho.y;
@@ -115,6 +138,9 @@ dvec2 RAYX_API getRefractiveIndex(double energy, int material) {
 
 // returns dvec2(atomic mass, density) extracted from materials.xmacro
 dvec2 RAYX_API getAtomicMassAndRho(int material) {
+    // This is an "X-Macro", see https://en.wikipedia.org/wiki/X_macro
+    // It allows us to generate a `case` for each material in the materials.xmacro file.
+    // The `case` matches upon the atomic number of the element, and returns the atomic mass and density as specified in the materials.xmacro file.
     switch (material) {
 #define X(e, z, a, rho) \
     case z:             \
