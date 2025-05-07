6f848e11feb413d9578d5681cbc3791745099ccb
Sven Erdem <svenerdem@gmail.com>
2025-05-07 10:21:04 +0200

    use __VA_OPT__ for device side throw library

--- Intern/rayx-core/src/Shader/Throw.h
+++ Intern/rayx-core/src/Shader/Throw.h
@@ -2,23 +2,23 @@
 
 #include "Core.h"
 
-#define _throw(string, ...) {                                                     \
-    printf("Error occurred while executing kernel: " string "\n", ##__VA_ARGS__); \
-    assert(false);                                                                \
-}
+#define _throw(string, ...) do {                                                              \
+    printf("Error occurred while executing kernel: " string "\n" __VA_OPT__(, ) __VA_ARGS__); \
+    assert(false);                                                                            \
+} while(0)
 
 #define _assert(condition, string, ...) \
     if (!(condition))                   \
-        _throw("assert(" #condition "): " string, ##__VA_ARGS__)
+        _throw("assert(" #condition "): " string  __VA_OPT__(, ) __VA_ARGS__)
 
 #define _warn(condition, string, ...)                                                  \
     if (!(condition))                                                                  \
-        printf("Warning occurred while executing kernel: " string "\n", ##__VA_ARGS__)
+        printf("Warning occurred while executing kernel: " string "\n" __VA_OPT__(, ) __VA_ARGS__)
 
 #ifdef RAYX_DEBUG_MODE
-#define _debug_throw_debug(string, ...) _throw(string, ##__VA_ARGS__)
-#define _debug_assert(condition, string, ...) _assert(condition, string, ##__VA_ARGS__)
-#define _debug_warn(condition, string, ...) _warn(condition, string, ##__VA_ARGS__)
+#define _debug_throw_debug(string, ...) _throw(string  __VA_OPT__(, ) __VA_ARGS__)
+#define _debug_assert(condition, string, ...) _assert(condition, string  __VA_OPT__(, ) __VA_ARGS__)
+#define _debug_warn(condition, string, ...) _warn(condition, string  __VA_OPT__(, ) __VA_ARGS__)
 #else
 #define _debug_throw(string, ...)
 #define _debug_assert(condition, string, ...)
