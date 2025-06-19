c2afe90164efde6e330a16da5efbdffbddc119ae
Sven Erdem <svenerdem@gmail.com>
2025-06-19 17:09:41 +0200

    fix potentially uninitialized variables

--- Intern/rayx-core/src/Beamline/LightSource.h
+++ Intern/rayx-core/src/Beamline/LightSource.h
@@ -18,6 +18,7 @@ enum class SourceDist { Uniform, Gaussian, Thirds, Circle };       // SourceDist
 enum class ElectronEnergyOrientation { Clockwise, Counterclockwise };
 enum class EnergySpreadUnit { EU_PERCENT, EU_eV };
 enum class SigmaType { ST_STANDARD, ST_ACCURATE };
+enum class SourcePulseType { None };
 
 struct DesignSource;
 
