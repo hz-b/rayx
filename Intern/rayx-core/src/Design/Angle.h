727cce89596e08611c92f9c1ff9dfad66878867b
Sven Erdem <svenerdem@gmail.com>
2026-01-13 01:14:14 +0100

    osteam operators and format for Angle

--- Intern/rayx-core/src/Design/Angle.h
+++ Intern/rayx-core/src/Design/Angle.h
@@ -31,11 +31,6 @@ inline Degrees toDegrees(const Angle angle) {
     return std::visit([](auto&& arg) { return toDegrees(arg); }, angle);
 }
 
-// TODO: write ostream operators and compatibility for std::format/std::print
-// std::ostream operator<<(std::ostream& os, const Degrees& angle) {
-//     return os << angle.value << "(deg)";
-// }
-
 namespace literals {
 
 inline Degrees operator"" _deg(long double value) { return Degrees{static_cast<double>(value)}; }
@@ -45,3 +40,60 @@ inline Radians operator"" _rad(long double value) { return Radians{static_cast<d
 }  // namespace literals
 
 }  // namespace rayx::design
+
+////////////////////////////////////////////////////////////
+// ostream operators
+////////////////////////////////////////////////////////////
+
+#include <ostream>
+
+inline std::ostream& operator<<(std::ostream& os, const rayx::design::Degrees degrees) {
+    return os << degrees.value << "(deg)";
+}
+
+inline std::ostream& operator<<(std::ostream& os, const rayx::design::Radians radians) {
+    return os << radians.value << "(rad)";
+}
+
+inline std::ostream& operator<<(std::ostream& os, const rayx::design::Angle angle) {
+    return std::visit([&os](const auto& arg) -> std::ostream& {
+        return os << arg;
+    }, angle);
+}
+
+////////////////////////////////////////////////////////////
+// std::format specializations
+////////////////////////////////////////////////////////////
+
+#include <format>
+
+template <>
+struct std::formatter<rayx::design::Degrees> : std::formatter<double> {
+    auto format(const rayx::design::Degrees degrees, auto& ctx) const {
+        auto out = std::formatter<double>::format(degrees.value, ctx);
+        return std::format_to(out, "(deg)");
+    }
+};
+
+template <>
+struct std::formatter<rayx::design::Radians> : std::formatter<double> {
+    auto format(const rayx::design::Radians radians, auto& ctx) const {
+        auto out = std::formatter<double>::format(radians.value, ctx);
+        return std::format_to(out, "(rad)");
+    }
+};
+
+template <>
+struct std::formatter<rayx::design::Angle> : std::formatter<double> {
+    auto format(const rayx::design::Angle angle, auto& ctx) const {
+        return std::visit([&]<typename T>(const T arg) {
+            auto out = std::formatter<double>::format(arg.value, ctx);
+
+            if constexpr (std::is_same_v<T, rayx::design::Degrees>) {
+                 return std::format_to(out, "(deg)");
+            } else {
+                 return std::format_to(out, "(rad)");
+            }
+        }, angle);
+    }
+};
