a751bf875d480fe136fef38b35ddc49f69f6afe1
fannyzotter <fanny.zotter@gmail.com>
2026-02-17 13:36:00 +0100

    Revert "Merge branch 'refactor/api_rewrite' of github.com:hz-b/RAY-X into refactor/api_rewrite2"
    This reverts commit 1bde26b8fb693e6ba462576c05eb912328540842, reversing
    changes made to 311370be28fde4629aab9bd3e2a933b0170d30d6.

--- Intern/rayx-core/src/Design/Object.h
+++ Intern/rayx-core/src/Design/Object.h
@@ -10,15 +10,5 @@ namespace rayx {
 
 using Object = std::variant<PointSource, MatrixSource, CircleSource, SimpleUndulatorSource, PixelSource, DipoleSource, InputSource, SurfaceElement>;
 
-using ObjectPtr =
-    std::variant<std::shared_ptr<PointSource>, std::shared_ptr<MatrixSource>, std::shared_ptr<CircleSource>, std::shared_ptr<SimpleUndulatorSource>,
-                 std::shared_ptr<PixelSource>, std::shared_ptr<DipoleSource>, std::shared_ptr<InputSource>, std::shared_ptr<SurfaceElement> >;
-
-namespace detail {
-// TODO: this is odd. it needs to be updated everytime a new element is added
-constexpr bool isElement(const ObjectPtr& object) {
-    return std::visit([]<typename T>(const std::shared_ptr<T>&) { return std::is_same_v<T, SurfaceElement>; }, object);
-}
-}  // namespace detail
-
-}  // namespace rayx
+using ObjectPtr = std::variant<
+    std::shared_ptr<PointSource>, std::shared_ptr<MatrixSource>, std::shared_ptr<CircleSource>, std::shared_ptr<SimpleUndulatorSource>, std::shared_ptr<PixelSource>, std::shared_ptr<DipoleSource>, std::shared_ptr<InputSource>, std::shared_ptr<SurfaceElement>, >;
